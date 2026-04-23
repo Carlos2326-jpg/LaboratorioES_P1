98604fe803d8abc0a0446159991fa9f146953e60
Carlos2326-jpg <joaocarlosdutra2326@gmail.com>
2026-04-23 19:42:05 -0300

    feat: Categoria adicionada ao Models

--- src/App/Models/Categoria.js
+++ src/App/Models/Categoria.js
@@ -1,9 +1,9 @@
 const mysql = require("mysql2/promise");
 
-class Cliente {
+class Categorias {
     constructor(dbConfig) {
         this.pool = mysql.createPool(dbConfig);
-        this.table = "Usuario";
+        this.table = "Categorias";
     }
 
     async all() {
@@ -13,42 +13,42 @@ class Cliente {
     }
 
     async find(id) {
-        const query = `SELECT * FROM ${this.table} WHERE idUsuario = ?`;
+        const query = `SELECT * FROM ${this.table} WHERE idCategoria = ?`;
         const [rows] = await this.pool.query(query, [id]);
         return rows[0];
     }
 
     async create(dados) {
-        const query = `INSERT INTO ${this.table} (nomeCompleto, email, senha, nivelAcesso) 
+        const query = `INSERT INTO ${this.table} (nome, slug, Descricao, imagemURL) 
                     VALUES (?, ?, ?, ?)`;
         const [result] = await this.pool.query(query, [
-            dados.nomeCompleto,
-            dados.email,
-            dados.senha,
-            dados.nivelAcesso
+            dados.nome,
+            dados.slug,
+            dados.Descricao,
+            dados.imagemURL
         ]);
         return { idUsuario: result.insertId, ...dados };
     }
 
     async update(id, dados) {
         const query = `UPDATE ${this.table} 
-                SET nomeCompleto = ?,
-                    email = ?,
-                    senha = ?,
-                    nivelAcesso = ?
-                WHERE idUsuario = ?`;
+                SET nome = ?, 
+                slug = ?, 
+                Descricao = ?, 
+                imagemURL = ?
+                WHERE idCategoria = ?`;
         const [result] = await this.pool.query(query, [
-            dados.nomeCompleto,
-            dados.email,
-            dados.senha,
-            dados.nivelAcesso,
+            dados.nome,
+            dados.slug,
+            dados.Descricao,
+            dados.imagemURL,
             id  
         ]);
         return result.affectedRows > 0;
     }
 
     async delete(id) {
-        const query = `DELETE FROM ${this.table} WHERE idUsuario = ?`;
+        const query = `DELETE FROM ${this.table} WHERE idCategoria = ?`;
         const [result] = await this.pool.query(query, [id]);
         return result.affectedRows > 0;
     }
