7238a1406588977f05dd774e4cb87e48db693e99
Carlos2326-jpg <joaocarlosdutra2326@gmail.com>
2026-04-23 18:08:57 -0300

    feat(database): adicionar tabela de Usuario_has_Postagem

--- blogAulasDB.sql
+++ blogAulasDB.sql
@@ -1,13 +1,14 @@
-create database BlogNoticias;
-use BlogNoticias;
+CREATE DATABASE BlogNoticias;
+
+USE BlogNoticias;
 
 -- Tabela Usuario
 CREATE TABLE Usuario (
     idUsuario INT PRIMARY KEY AUTO_INCREMENT,
     nomeCompleto VARCHAR(45) NOT NULL,
     email VARCHAR(45) NOT NULL UNIQUE,
-    senha VARCHAR(60) NOT NULL, 
-    nivelAcesso ENUM('admin', 'editor', 'Leitor', 'Autor')
+    senha VARCHAR(60) NOT NULL,
+    nivelAcesso ENUM ('admin', 'editor', 'Leitor', 'Autor')
 );
 
 -- Tabela Categorias
@@ -17,7 +18,7 @@ CREATE TABLE Categorias (
     slug VARCHAR(45) NOT NULL UNIQUE,
     Descricao VARCHAR(45),
     imagem BLOB,
-    status ENUM('ativo', 'inativo') DEFAULT 'ativo'
+    status ENUM ('ativo', 'inativo') DEFAULT 'ativo'
 );
 
 -- Tabela Postagem
@@ -30,14 +31,23 @@ CREATE TABLE Postagem (
     img BLOB,
     dataPostagem DATETIME DEFAULT CURRENT_TIMESTAMP,
     Usuario_idUsuario INT NOT NULL,
-    FOREIGN KEY (Usuario_idUsuario) REFERENCES Usuario(idUsuario) ON DELETE CASCADE
+    FOREIGN KEY (Usuario_idUsuario) REFERENCES Usuario (idUsuario) ON DELETE CASCADE
 );
 
--- Tabela de relacionamento N:N entre Categorias e Postagem
+-- Tabela N:N — Categorias e Postagem
 CREATE TABLE Categorias_has_Postagem (
     Categorias_idCategoria INT NOT NULL,
     Postagem_idPostagem INT NOT NULL,
     PRIMARY KEY (Categorias_idCategoria, Postagem_idPostagem),
-    FOREIGN KEY (Categorias_idCategoria) REFERENCES Categorias(idCategoria) ON DELETE CASCADE,
-    FOREIGN KEY (Postagem_idPostagem) REFERENCES Postagem(idPostagem) ON DELETE CASCADE
+    FOREIGN KEY (Categorias_idCategoria) REFERENCES Categorias (idCategoria) ON DELETE CASCADE,
+    FOREIGN KEY (Postagem_idPostagem) REFERENCES Postagem (idPostagem) ON DELETE CASCADE
+);
+
+-- Tabela N:N — Usuario e Postagem
+CREATE TABLE Usuario_has_Postagem (
+    Usuario_idUsuario INT NOT NULL,
+    Postagem_idPostagem INT NOT NULL,
+    PRIMARY KEY (Usuario_idUsuario, Postagem_idPostagem),
+    FOREIGN KEY (Usuario_idUsuario) REFERENCES Usuario (idUsuario) ON DELETE CASCADE,
+    FOREIGN KEY (Postagem_idPostagem) REFERENCES Postagem (idPostagem) ON DELETE CASCADE
 );
